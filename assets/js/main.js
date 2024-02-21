506ea5d72b1b4642285fae0c428c88948a078e78
MarioBroi <inoix16@gmail.com>
2024-02-21 14:22:49 +0000

    add if to check the multiples in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 //console.log('Test');
 
 for (let n = 1; n <= 100; n++) {
-    console.log(n); //generated numbers from 1 to 100
+    //console.log(n); //generate numbers from 1 to 100
+
+    if (n % 3 == 0 && n % 5 == 0) {
+        console.log('FizzBuzz'); //'FizzBuzz'
+
+    } else if (n % 3 == 0) {
+        console.log('Fizz'); //'Fizz'
+
+    } else if (n % 5 == 0) {
+        console.log('Buzz'); //'Buzz'
+
+    } else {
+        console.log(n); //Number
+    }
 
 }
